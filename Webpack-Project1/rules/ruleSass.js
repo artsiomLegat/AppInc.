3fb3400160d2fc900ff26425c275b0a9ac07d82f
artsiomLegat <artem-1995effect@mail.ru>
2020-01-19 11:10:05 +0300

    test brach (AnotherBranch)

---
+++ Webpack-Project1/rules/ruleSass.js
@@ -0,0 +1,6 @@
+const loaderCss = require('../loaders/loaderCss');
+const loaderStyle = require('../loaders/loaderStyle');
+const loaderPostcss = require('../loaders/loaderPostcss');
+const loaderExtractCss = require('../loaders/loaderExtractCss');
+
+const {sourcePath,styleSassPath} = require('../utils/paths');
