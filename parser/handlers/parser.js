aacc4fe71fee11f66d7bdaa3e297c30bbe20b789
artsiomLegat <artem-1995effect@mail.ru>
2020-04-03 14:42:12 +0300

    soSo

--- parser/handlers/parser.js
+++ parser/handlers/parser.js
@@ -13,7 +13,9 @@ const LAUNCH_PUPPETEER_OPTS = {
 
 const PAGE_PUPPETEER_OPTS = {
     networkIdle2Timeout: 5000,
-    waitUntil: 'networkidle2',
+    waitUntil: {
+        waitUntil: ['load','domcontentloaded','networkidle0','networkidle2']
+    },
     timeout: 3000000
   };
 
@@ -21,14 +23,44 @@ const PAGE_PUPPETEER_OPTS = {
 
  module.exports = class getHTMLinfo {
     constructor(){
-        this.browser = this.browser.bind(this,async ()=>(await poppeteer.launch(LAUNCH_PUPPETEER_OPTS))); 
+        // this.init = function(initBrowser){return initBrowser};
+        // this.brows =  this.init.bind(this,(async ()=>(await poppeteer.launch(LAUNCH_PUPPETEER_OPTS)))()); 
+        // this.browser = async function(){
+        //     try {
+        //         return await poppeteer.launch(LAUNCH_PUPPETEER_OPTS);
+        //     }
+        //      catch (e){
+        //         throw new Error(chalk.red(e.message));
+        //      }
+        // };
+        this.initBrowser =this.initBrowser.bind(this);
+        this.browser = null;
     };
+   async initBrowser (){
+    this.browser = await poppeteer.launch(LAUNCH_PUPPETEER_OPTS);
+    }
    async getPageContent(url) {
        try{
+        await this.initBrowser();
         let page = await this.browser.newPage();
         await page.goto(url,PAGE_PUPPETEER_OPTS);
+        // await page.evaluate(() => {
+        //     document.querySelector('a[id="nav-hamburger-menu"]').click();
+        //   });
+    
+        // await page.setViewport({ width: 1868, height: 978 })
+  
+        // await page.waitForSelector('#navbar > #nav-belt > .nav-left > #nav-hamburger-menu > .hm-icon');
+        // let pageclick =  await page.click('#navbar > #nav-belt > .nav-left > #nav-hamburger-menu > .hm-icon');
+        //  await page.waitFor(6000);
+        // page.waitForNavigation()
+        // let elem = await page.$('#hmenu-content');
+        // let selector = await page.$('div["id=hmenu-customer-profile"]');вв
+      
+        // let elem = selector.asElement();
+        // await elem.click()
         let content = await page.content();
-        return content;
+        return  content;
        } catch(e) {
          throw new Error(e.message);
        }
--- parser/start.js
+++ parser/start.js
@@ -1,12 +1,20 @@
 const cheerio = require('cheerio');
 const getHTMLinfo = require('./handlers/parser.js');
 const URL = 'https://www.amazon.com';
+const chalk = require('chalk');
 
 let pupp = new getHTMLinfo();
 
 (async function main(){
- let HTMLcontent = pupp.getHtmlInfo(URL);
- let $ = cheerio.load(HTMLcontent);
- let attrClass = $('.nav-fill').attr('class');
- console.log($('.nav-fill').attr('class'));
-})
+    try{
+        let HTMLcontent = await pupp.getPageContent(URL);
+        // let $ = cheerio.load(HTMLcontent);
+        // let attrClass = $('#hmenu-content').attr('id');
+        console.log(HTMLcontent);
+        // console.log(HTMLcontent);
+        // console.log($);выф
+    }
+     catch(e){
+         throw new Error(chalk.red(e.message));
+     }
+})()
