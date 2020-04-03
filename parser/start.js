132558b599d4c9376e033f566c4aae95fdeed0e6
artsiomLegat <artem-1995effect@mail.ru>
2020-04-03 12:20:15 +0300

    somesLes

--- parser/start.js
+++ parser/start.js
@@ -8,5 +8,10 @@ let pupp = new getHTMLinfo();
  let HTMLcontent = await pupp.getPageContent(URL);
  let $ = cheerio.load(HTMLcontent);
  let attrClass = $('.nav-fill').attr('class');
+<<<<<<< HEAD
  console.log(HTMLcontent);
 })();
+=======
+ console.log($('.nav-fill').attr('class'));
+})
+>>>>>>> 35fab51b82b2c9697c61a1d477b487c7623df172
